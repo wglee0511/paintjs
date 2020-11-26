41f5dd3248fcdcf709a034b797fa34ff4be67129
wglee0511 <ranazang@naver.com>
2020-11-26 12:07:57 +0900

    canvas,"path", "line"

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c"; 
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
-const stopPainting = (event) => {
+
+const stopPainting = () => {
     painting = false;
 }
 
 const onMouseMove = (event) => {
     const x = event.offsetX;
     const y = event.offsetY;
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+    } else {
+        ctx.lineTo(x,y);
+        ctx.stroke();
+        
+    }
 }
 
-const onMouseDown = (event) => {
+const startPainting = () => {
     painting = true;
 }
 
-const onMouseUp = (event) => {
-    stopPainting();
-}
-
-
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
